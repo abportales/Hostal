6fd6ab69e3fb0b6aed6721ad15b02b1a8fd4376b
abportales <aportales@revoktek.com>
2023-03-31 15:57:57 -0600

    se crea table room para prueba de crud generator

---
+++ app/Http/Requests/Admin/Room/UpdateRoom.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Admin\Room;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
+
+class UpdateRoom extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('admin.room.edit', $this->room);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['nullable', 'string'],
+            'floor' => ['nullable', 'string'],
+            'price' => ['nullable', 'numeric'],
+            'rented' => ['nullable', 'boolean'],
+            
+        ];
+    }
+
+    /**
+     * Modify input data
+     *
+     * @return array
+     */
+    public function getSanitized(): array
+    {
+        $sanitized = $this->validated();
+
+
+        //Add your code for manipulation with request data here
+
+        return $sanitized;
+    }
+}
